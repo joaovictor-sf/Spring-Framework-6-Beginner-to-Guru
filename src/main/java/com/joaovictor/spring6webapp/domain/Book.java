1975ee431c0434b3db10328fc7dd59c11beeb0c8
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2023-08-23 19:28:58 -0300

    first commit

---
+++ src/main/java/com/joaovictor/spring6webapp/domain/Book.java
@@ -0,0 +1,54 @@
+package com.joaovictor.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+    //Alguns prefeririam usar List, mas List possibilidade elementos repeditos,
+    // e cada elemento irá ser único, então ultilizaremos Set
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
