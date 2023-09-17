2a6b86ec7e5e497ddf2f261c80286ea6c2639c22
João Victor Santana Ferreira <joaovictorsantanaf@gmail.com>
2023-09-17 07:21:41 -0300

    test context

--- spring-6-dl/src/test/java/com/joaovictor/spring6dl/Spring6DlApplicationTests.java
+++ spring-6-dl/src/test/java/com/joaovictor/spring6dl/Spring6DlApplicationTests.java
@@ -1,10 +1,31 @@
 package com.joaovictor.spring6dl;
 
+import com.joaovictor.spring6dl.controller.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DlApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowiredOfController() {
+		System.out.println(myController.hello());
+	}
+
+	@Test
+	void testGetControllerFromContext() {
+		MyController controller = (MyController) applicationContext.getBean("myController");
+
+		System.out.println(controller.hello());
+	}
 
 	@Test
 	void contextLoads() {
